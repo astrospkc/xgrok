318df3219ee7bb8433c664747cb33ec45309f821
astrospkc <punamkumari399@gmail.com>
2023-12-03 11:34:06 +0530

    first commit - adding all the files

---
+++ src/app/upload-button/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UploadButtonPge = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default UploadButtonPge;
---
+++ src/app/upload-dnd/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UploadDragDrop = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default UploadDragDrop;
